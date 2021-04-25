24f91eb09f049f7cae1bea41be9da33db3f98d38
anmarkosyan <anush.markos@gmail.com>
2021-04-25 21:07:41 +0400

    coding challenge

--- 05-Code-Quality/script.js
+++ 05-Code-Quality/script.js
@@ -1,59 +1,89 @@
 'use strict';
 
-function x(n, l, a, e, r) {
-  let s,
-    t = null != l ? l : n.nullContext || {};
+//======================== 👩🏻‍💻 coding challenge ======================
+//select elements
+const displayUI = document.querySelector('body');
 
-  return (
-    '<li class="dropdown">\n' +
-    ' <a href="#" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle" data-toggle="dropdown">\n' +
-    '<i class="user-avatar fa fa-user bg-primary"></i>\n' +
-    '<span class="caret"></span>\n' +
-    '</a>\n' +
-    '<ul class="dropdown-menu" role="menu" id="navDropdownMenu">\n\n' +
-    '<li ' +
-    (null != (s = a.call(t, null != (s = null != l ? l.user : l) ? s.href : s, { name: 'unless', hash: {}, fn: n.program(21, r, 0), inverse: n.noop, data: r }))
-      ? s
-      : '') +
-    '>' +
-    '\x3c!-- dropdown-no-link--\x3e\n' +
-    (null !=
-    (s = a['if'].call(t, null != (s = null != l ? l.user : l) ? s.href : s, { name: 'if', hash: {}, fn: n.program(23, r, 0), inverse: n.noop, data: r }))
-      ? s
-      : '') +
-    '                                ' +
-    '' +
-    (null !=
-    (s = a['if'].call(t, null != (s = null != l ? l.user : l) ? s.label : s, { name: 'if', hash: {}, fn: n.program(25, r, 0), inverse: n.noop, data: r }))
-      ? s
-      : '') +
-    '\n ' +
-    '<strong id="user_name">' +
-    n.escapeExpression(n.lambda(null != (s = null != l ? l.user : l) ? s.username : s, l)) +
-    '</strong>\n                                ' +
-    '' +
-    (null !=
-    (s = a['if'].call(t, null != (s = null != l ? l.user : l) ? s.secondaryLabel : s, {
-      name: 'if',
-      hash: {},
-      fn: n.program(27, r, 0),
-      inverse: n.noop,
-      data: r,
-    }))
-      ? s
-      : '') +
-    '\n' +
-    (null !=
-    (s = a['if'].call(t, null != (s = null != l ? l.user : l) ? s.href : s, { name: 'if', hash: {}, fn: n.program(29, r, 0), inverse: n.noop, data: r }))
-      ? s
-      : '') +
-    ' ' +
-    ' </li>\n\n' +
-    '<li role="separator" class="divider"></li>\n\n' +
-    (null != (s = a.call(t, null != l ? l.userBar : l, { name: 'each', hash: {}, fn: n.program(31, r, 0), inverse: n.noop, data: r })) ? s : '') +
-    ' ' +
-    '</ul>\n</li>\n'
-  );
+//Functions
+const data = {};
+
+function currentStudent(usersData, password, email, payment = true, countOfCourses) {
+  let currentUser;
+  const currentId = null !== password ? password : usersData.nullContext || {};
+
+  const html =  `
+    <li class="dropdown">
+       <a href="#" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle" data-toggle="dropdown">
+          <i class="user-avatar fa fa-user bg-primary"></i> <span class="caret"></span>
+       </a>
+          <ul class="dropdown-menu" role="menu" id="navDropdownMenu">\n\n
+             <li>
+                ${null !== (currentUser = email.call(currentId, null !== (currentUser = null !== password ? password.user : password) ? currentUser.href : currentUser, {
+                name: 'unless',
+                hash: {},
+                fn: usersData.program(21, countOfCourses, 0),
+                inverse: usersData.noop,
+                data: countOfCourses,
+                })) ? currentUser : ''} 
+                
+                 \x3c!-- dropdown-no-link--\x3e
+               ${null !==
+                (currentUser = email['if'].call(currentId, null !== (currentUser = null !== password ? password.user : password) ? currentUser.href : currentUser, {
+                name: 'if',
+                hash: {},
+                fn: usersData.program(23, countOfCourses, 0),
+                inverse: usersData.noop,
+                data: countOfCourses,
+                })) ? currentUser : ''}
+    
+               ${null !==
+                (currentUser = email['if'].call(currentId, null !== (currentUser = null !== password ? password.user : password) ? currentUser.label : currentUser, {
+                name: 'if',
+                hash: {},
+                fn: usersData.program(25, countOfCourses, 0),
+                inverse: usersData.noop,
+                data: countOfCourses,
+                })) ? currentUser : ''} 
+               
+                   <strong id="user_name">
+                      ${usersData.escapeExpression(usersData.lambda(null !== (currentUser = null !== password ? password.user : password) 
+                      ? currentUser.username : currentUser, password))}
+                   </strong>
+                                    
+               ${null !== (currentUser = email['if'].call(currentId, null !== (currentUser = null !== password ? password.user : password) 
+               ? currentUser.secondaryLabel : currentUser, { 
+                 name: 'if',
+                 hash: {},
+                 fn: usersData.program(27, countOfCourses, 0),
+                 inverse: usersData.noop,
+                 data: countOfCourses, })) ? currentUser : ''} 
+    
+              ${null !== (currentUser = email['if'].call(currentId, null !== (currentUser = null !== password ? password.user : password) 
+               ? currentUser.href : currentUser, {
+                name: 'if',
+                hash: {},
+                fn: usersData.program(29, countOfCourses, 0),
+                inverse: usersData.noop,
+                data: countOfCourses, })) ? currentUser : ''} 
+             </li>\n\n 
+    
+             <li role="separator" class="divider"></li>
+                ${null !== (currentUser = email.call(currentId, null !== password ? password.userBar : password, {
+                name: 'each',
+                hash: {}, 
+               fn: usersData.program(31, countOfCourses, 0),
+               inverse: usersData.noop,
+               data: countOfCourses, })) ? currentUser : ''} 
+         </ul>
+   </li>
+  `;
+ return displayUI.innerHTML = html;
+
 }
 
-console.log(x(1, 2, 3, 4, 5));
+//event listeners
+document.addEventListener('click', e => {
+  e.preventDefault();
+
+  currentStudent(data, 1234567, 'email@com', true, 5);
+});
