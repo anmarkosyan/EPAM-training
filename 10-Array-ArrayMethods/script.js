47e520ea8cb8a5537d74bf4190ce63ac2f268711
anmarkosyan <anush.markos@gmail.com>
2021-05-05 12:14:59 +0400

    added new field

---
+++ 10-Array-ArrayMethods/script.js
@@ -0,0 +1,7 @@
+'use strict';
+//======================= 🔴 Array ========================
+const arr = [1, 2, 3];
+const arr1 = arr.slice()
+
+console.log(arr === arr1);//false
+//====================== 👩🏻‍💻 coding challenge =============
