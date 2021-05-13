b49bf2baa889177ff4dea5b4b9376cfd9e718ce3
anmarkosyan <anush.markos@gmail.com>
2021-05-13 23:01:56 +0400

    solving homework

--- 12-ObjectMethods-Destructuring/script.js
+++ 12-ObjectMethods-Destructuring/script.js
@@ -2,6 +2,7 @@
 
 //============================== 🔴 Lecture part ============================
 //✅ destructuring
+/*
 const obj = { name: 'Anush', age: 34 };
 //❗️console.log(...obj);//TypeError: Found non-callable @@iterator
 //❗️console.log(`${...obj}`);//SyntaxError: Unexpected token '...'
@@ -28,211 +29,8 @@ const map = new Map([
   ['age', 12],
 ]);
 console.log(...map); //['name', 'Aram'] ['age', 12 ]
-
+ */
 //========================= 👩🏻‍💻 coding challenges =========================
-//🏋🏻‍♀️
-// Number.prototype.plus = function (number) {
-//   return this + number;
-// };
-//
-// Number.prototype.minus = function (number) {
-//   return this - number;
-// };
-// console.log((5).plus(3).plus(3).minus(2));
-
-//🏋🏻‍♀️
-/*There is a salaries object with arbitrary number of salaries.
-Write the function sumSalaries(salaries) that returns the sum of all salaries using Object.values and the for..of loop.
-If salaries is empty, then the result must be 0
-*/
-
-// const salaries = {
-//   John: 100,
-//   Pete: 300,
-//   Mary: 250,
-// };
-//
-// const sumSalaries = function (obj) {
-//   let sum = 0;
-//   for (const value of Object.values(obj)) {
-//     sum += value;
-//   }
-//   return sum;
-//
-//   // return Object.values(obj).reduce(function(acc, num){
-//   //   acc += num;
-//   //   return acc;
-//   // }, 0)
-// };
-// console.log(sumSalaries(salaries)); //650
-
-//🏋🏻‍♀️
-//Write a function count(obj) that returns the number of properties in the object:
-// let user = {
-//   name: 'John',
-//   age: 30,
-// };
-//
-// const count = function (obj) {
-//   return Object.keys(obj).length;
-// };
-//
-// console.log(count(user)); // 2
-
-//🏋🏻‍♀️
-
-// const curry = {
-//   fName: "Steph",
-//   lName:"Curry",
-//   number:30,
-//   team:"Warriors",
-//   college:"Davidson"};
-//
-// function getCollege(player){
-//   return player.college;
-//
-// }
-//
-// console.log(getCollege(curry));
-
-//🏋🏻
-/*‍♀️
-function NBAPlayer(name, team, ppg){
-  this.name=name;
-  this.team=team;
-  this.ppg=ppg;
-}
-
-const iverson = new NBAPlayer("Iverson", "76ers", 11.2);
-const jordan = new NBAPlayer("Jordan", "bulls", 20.2);
-
-
-const sumPPG = function(player1, player2){
-  return player1.ppg + player2.ppg
-
-};
-
-console.log(sumPPG(iverson, jordan));// => 31.4
- */
-
-//🏋🏻‍♀️
-/*
-function countLetters(str) {
-  const newStr = str.toLowerCase();
-  // const obj = {};
-  // for (const el of newStr) {
-  //   if (el !== ' ' && !obj[el]) {
-  //     obj[el] = 1;
-  //   } else if (obj[el]) {
-  //     obj[el]++;
-  //   }
-  // }
-  //
-  // return obj;
-
-  return [...newStr].reduce(function(acc, el){
-    if(el !== ' ' && !acc[el]) {
-          acc[el] = 1;
-        } else if (acc[el]) {
-          acc[el]++;
-        }
-    return acc;
-  }, {})
-}
-
-console.log(countLetters('school')); //{s: 1, c: 1, h: 1, o: 2, l: 1}
-console.log(countLetters('I like milk')); //{i: 3, l: 2, k: 2, e: 1, m: 1}
- */
-
-//🏋🏻‍♀️
-// function countWords(str) {
-//   const lowerStr = str.toLowerCase();
-//   return lowerStr.split(' ').reduce(function (acc, word) {
-//     if (!acc[word]) acc[word] = 1;
-//     else acc[word]++;
-//     return acc;
-//   }, {});
-// }
-//
-// console.log(countWords('A life is a moment')); //{a: 2, life: 1, is: 1, moment: 1}
-
-//🏋🏻‍♀️
-/*
-function LivingThing(name, race) {
-  this.name = name;
-  this.race = race;
-}
-const player = new LivingThing('Gary', 'human');
-const plant = new LivingThing('Carrot', 'plant');
-
-const bite = function (thing) {
-  // if (thing.race === 'human') thing.race = 'zombie';
-  //
-  // return thing;
-
-  thing.race === 'human' ? (thing.race = 'zombie') : thing.race;
-  return thing;
-};
-console.log(bite(player));
-console.log(bite(plant));
- */
-
-//🏋🏻‍
-// function mostFrequentElement(arr) {
-//   const obj = arr.reduce(function (acc, num) {
-//     if (!acc[num]) acc[num] = 1;
-//     else acc[num]++;
-//     return acc;
-//   }, {});
-//
-//   const maxNum = Math.max(...Object.values(obj));
-//
-//   return Object.keys(obj).reduce(function (acc, num) {
-//     if (obj[num] === maxNum) acc.push(+num);
-//     return acc;
-//   }, []);
-// }
-//
-// console.log(mostFrequentElement([5, 2, 1, 5])); //[5]
-// console.log(mostFrequentElement([5, 7, 1, 1, 1, 8, 7, 3, 6, 7, 1])); //[1]
-// console.log(mostFrequentElement([5, 7, 7, 1, 1, 1, 8, 7, 3, 6, 7, 1])); //[1, 7]
-
-//🏋🏻‍♀️
-/*
-const Ball = function (ballType = 'regular') {
-  this.ballType = ballType;
-};
-
-console.log(new Ball().ballType);//'regular'
-console.log(new Ball('super').ballType);//'super'
-*/
-//🏋🏻‍♀️
-// String.prototype.isUpperCase = function () {
-//   return this.toString() === this.toUpperCase();
-// };
-//
-// console.log('c'.isUpperCase()); //false
-// console.log('C'.isUpperCase()); // == true
-// console.log('-------');
-// console.log('#LOVEWINS'.isUpperCase());
-// console.log('hello I AM DONALD'.isUpperCase()); // == false
-// console.log('HELLO I AM DONALD'.isUpperCase()); // == true
-// console.log('ACSKLDFJSgSKLDFJSKLDFJ'.isUpperCase()); // == false
-// console.log('ACSKLDFJSGSKLDFJSKLDFJ'.isUpperCase()); // == true
-
-//🏋🏻‍♀️
-//checker only for 0-9 digits
-/*
-String.prototype.digit = function() {
-  return /^\d$/g.test(this);
-};
-console.log(''.digit());//false
-console.log('7'.digit());//true
-console.log('12'.digit());//false
-console.log('2ac'.digit());//false
-
-console.log(+'');
- */
 //🏋🏻‍♀️
 /*
 Create a vehicle object with the following properties using function constructor:
@@ -251,31 +49,37 @@ Create a vehicle object with the following properties using function constructor
 Function constructor should get one argument as an object.
 All the bold italic properties should be optional (you should assign default values for them).
 Use object destructuring  in function argument and initialize vehicle (this) object.
+
 Add a method for the object called getTiersInfo which should return a descriptive string
 consist of destructured properties about tiers nested object.
 
  You can also add a couple of other methods using destructuring (up to you :))
 */
 
-const vehicle = {
-  model: 'Mini Cooper',
-  manufacturer: 'England',
-  country: 'European',
-  power: '',
-  run: '',
-  fuelConsumption: '',
-  color: '',
-  tiers: {
-    qty: 0,
-    diameter: 0,
-    model: '',
-    run: 123,
-  },
-};
-const Vehicle = function (obj) {
-   this.obj = obj;
-};
+const Vehicle = function (model, manufacturer, country, run, power, fuelConsumption, color, ...arg) {
+  this.carModel = model;
+  this.manufacturer = manufacturer;
+  this.country = country;
+  this.power = power;
+  this.run = run || 90;
+  this.fuelConsumption = fuelConsumption || 'petrol';
+  this.color = color;
+  this.tiers = { ...arg };
 
-const vehicle1 = new Vehicle(vehicle);
-//console.log(vehicle1);
-
+  this.getTiersInfo = function () {
+    const {
+      carModel,
+      country,
+      color,
+      fuelConsumption,
+      tiers: { 0: qty, 1: diameter, 2: model, 3: run },
+    } = this;
+    return `🚙 Car description: \n ${carModel} - ${model}\n country: ${country}\n color: ${color} \n diameter: ${diameter}\n quantity: ${qty}\n run: ${run} \n fuel consumption: ${fuelConsumption}`;
+  };
+};
+const miniCooper = new Vehicle('Mini Cooper', 'England', 'Netherlands', undefined, 120.7, undefined, 'chestnut', 1, 65, 'K10', 120);
+const lexus = new Vehicle('Lexus', 'Japan', 'Japan', 120, 130, 'hybrid', 'amber shine', 1, 90, 'RX 350', 160);
+console.log(miniCooper);
+console.log(lexus);
+console.log(miniCooper.getTiersInfo());
+console.log(lexus.getTiersInfo());
