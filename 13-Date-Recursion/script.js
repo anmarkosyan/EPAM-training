4edee8970dda9dec5054fd94765ba6fd3980320b
anmarkosyan <anush.markos@gmail.com>
2021-05-18 20:47:29 +0400

    find missing number with XOR operator

--- 13-Date-Recursion/script.js
+++ 13-Date-Recursion/script.js
@@ -110,7 +110,7 @@ Write a function that finds last deeper property of the object and return as a s
 // console.log(findDeeperProp({ x: 5, y: { z: 6, k: 4, m: { p: 1 } } })); // -> 'p:1'
 // console.log(findDeeperProp({ x: 3, y: { c: { b: 2 } } })); // 'b: 2'
 // console.log(findDeeperProp({})); //''
-
+/*
 let count1 = 0;
 let count2 = 0;
 
@@ -190,29 +190,57 @@ console.log(
     a: { m: { n: 12 }, k: { t: { l: 28 } }, b: 1, c: 2 },
   })
 );
-
+ */
 //3️⃣
 /*
 you have number from 1 to 100 in an array const arrayOfNumbers = [5,2,77,34…]  (note without order) and there missed one item,
 we need to find this missed number.
 Note: you can’t plus arrayOfNumbers numbers to each other and subtract from 1-100 added result, there is expected another approach to solve it.
 */
-
-// const findMissedNumber = function (arr) {
-//   if (arr.length === 0) return arr;
-//   const orderedArr = arr.slice().sort((a, b) => a - b);
+// if (arr.length === 0) return arr;
+// const orderedArr = arr.slice().sort((a, b) => a - b);
 //
-//   for (let i = 0; i < orderedArr.length / 2; i++) {
-//     if (orderedArr[i] + 1 !== orderedArr[i + 1]) {
-//       return orderedArr[i] + 1;
-//     }
+// for (let i = 0; i < orderedArr.length - 1; i++) {
+//   if (orderedArr[i] + 1 !== orderedArr[i + 1]) {
+//     return orderedArr[i] + 1;
 //   }
-//   return '❗️ no missed number';
-// };
-// console.log(findMissedNumber([6, 4, 1, 2, 7, 3])); //5
-// console.log(findMissedNumber([])); //[]
-// console.log(findMissedNumber([3, 4, 5, 1, 7, 2, 8, 6])); //'❗️ no missed number'
-// console.log(findMissedNumber([]));
+// }
+// return '❗️ no missed number';
+//2 way://Time complexity 0(N)/0(1)
+// const n = arr.length + 1;
+// let sum = 0;
+//
+// for (let i = 1; i <= n; i++) {
+//   sum += i;
+// }
+// console.log(sum);//10
+// arr.forEach(function(num){
+//   sum -= num;
+//
+// })
+// return sum;
+
+//3way: XOR logic operator
+const findMissedNumber = function (arr) {
+  const maxNum = arr.length + 1;
+  //find sum of all numbers with comparing digits from 1 to given array length + 1
+  let xor1 = 1;
+  for (let i = 2; i <= maxNum; i++) {
+    xor1 = xor1 ^ i;
+  }
+
+  //find sum of all numbers with comparing from a first num until the last number of a given array
+  let xor2 = arr[0];
+  for (let i = 1; i < maxNum - 1; i++) {
+    xor2 = xor2 ^ arr[i];
+  }
+
+  //and finally compare the two results,
+  return xor1 ^ xor2;
+};
+
+console.log(findMissedNumber([2, 3, 1, 5, 6, 7])); //4
+console.log(findMissedNumber([5, 2, 3, 1, 4, 7, 8, 10, 9, 13, 12, 11, 14])); //6
 
 // function goDeeper(obj, count = 1) {
 //   const arr = [];
