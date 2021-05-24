48fe2f367d1a381c625fe7e4bf00dd5c25deb768
anmarkosyan <anush.markos@gmail.com>
2021-05-24 08:45:30 +0400

    IIFE tasks

--- 16-new_Function-IIFE/script.js
+++ 16-new_Function-IIFE/script.js
@@ -1,1 +1,38 @@
 'use strict';
+//================= 🔴 lecture ========================
+//1️⃣
+// (function immediateA(a) {
+//   return (function immediateB(b) {
+//     console.log(a); // 0
+//   })(1);
+// })(0);
+//
+// //2️⃣
+// let count = 0;
+// (function immediate() {
+//   if (count === 0) {
+//     let count = 1;
+//     console.log(count); // 1
+//   }
+//   console.log(count); // 0
+// })();
+
+//3️⃣
+// var i = 0;
+// for (; i < 3; i++) {
+//   setTimeout(function log() {
+//     console.log(i); // 3 3 3
+//   }, 1000);
+// }
+
+//4️⃣
+// for (var i = 0; i < array.length; i++) {
+//   (function (num) {
+//     setTimeout(function () {
+//       console.log(`Element: ${arr[num]}, at index:  ${num}`);
+//     }, 1000);
+//   })(i);
+// }
+
+
+//================= 🔴 coding challenges ==============
