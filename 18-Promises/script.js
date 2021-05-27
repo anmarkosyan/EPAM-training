23249ccb0e8466ebfd071c95191126463d23d03b
anmarkosyan <anush.markos@gmail.com>
2021-05-27 22:55:21 +0400

    added helper function for manually errors

--- 18-Promises/script.js
+++ 18-Promises/script.js
@@ -2,23 +2,32 @@
 //=============== 🔴 lecture part ===================
 const btn = document.querySelector('.btn-country');
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(`${errorMsg} (${response.status})!!!`);
+    }
+    return response.json();
+  });
+};
+
 const getCountryData = function (country) {
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then(response => response.json())
+   getJSON(`https://restcountries.eu/rest/v2/name/${country}`, 'Country not found')
     .then(data => {
       console.log(data[0]);
       const neighbour = data[0].borders[1];
 
-      return fetch(`https://restcountries.eu/rest/v2/name/${neighbour}`);
+      if(!neighbour) throw new Error('No neighbour found 🤷🏻‍')
+      return getJSON(`https://restcountries.eu/rest/v2/name/${neighbour}`, 'Country not found');
     })
-    .then(response => response.json())
     .then(data => console.log(data[0]))
     .catch(err => {
-      console.error(`Something went wrong 💥💥💥 ${err.message}.Try again!`);
+      console.error(`Something went wrong 💥💥💥 ${err.message} .Try again!`);
     });
 };
 
 btn.addEventListener('click', function () {
-  getCountryData('armenia');
+  getCountryData('australia');
 });
+//getCountryData('hjkghj');
 //================ 🔴 coding challenges =============
