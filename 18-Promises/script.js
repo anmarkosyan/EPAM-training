16c64a68b84e2040658f9207ae40133f961511c6
anmarkosyan <anush.markos@gmail.com>
2021-05-27 21:20:32 +0400

    createing promising chaining

--- 18-Promises/script.js
+++ 18-Promises/script.js
@@ -2,16 +2,18 @@
 //=============== 🔴 lecture part ===================
 // const request = fetch('https://restcountries.eu/rest/v2/name/portugal');
 // console.log(request);//Promise{}
-console.log(window.fetch('hi'));
+
 const getCountryData = function (country) {
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then(response => {
-      console.log(response); // Response{}
-      return response.json();
+    .then(response => response.json())
+    .then(data => {
+      console.log(data[0]);
+      const neighbour = data[0].borders[1];
+
+      return fetch(`https://restcountries.eu/rest/v2/name/${neighbour}`);
     })
-    .then(response => {
-      console.log(response);
-    });
+    .then(response => response.json())
+    .then(data => console.log(data[0]));
 };
 getCountryData('armenia');
 //================ 🔴 coding challenges =============
