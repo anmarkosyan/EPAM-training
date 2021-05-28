2c1ed995b89f13e4d5698b95694511d55e58220f
anmarkosyan <anush.markos@gmail.com>
2021-05-28 22:35:30 +0400

    added some new tasks

--- 17-Timer/script.js
+++ 17-Timer/script.js
@@ -1,8 +1,7 @@
 'use strict';
-//====================== Timer ====================
-
+//====================== 👩🏻‍💻 coding challenges => Timer ====================
 //console.log(window.setTimeout);//global object method
-
+//1️⃣
 // const timer = setTimeout(function () {
 //   console.log('hi');
 // }, 10000);
@@ -11,14 +10,75 @@
 //   clearTimeout(timer);
 //   console.log('stop timer!!');
 // }, 5000);
+//2️⃣
+// for (var i = 0; i < 10; i++) {
+//   setTimeout(
+//     num => {
+//       console.log(num);
+//     },
+//     0,
+//     i
+//   );
+// }
+//0 1 2 3 4 5 6 7 8 9
 
-//======
+//3️⃣
+// for (let i = 0; i < 10; i++) {
+//   const start = Date.now();
+//   setTimeout(() => {
+//     console.log(Date.now() - start);
+//   }, 1500);
+// }
 
-for (var i = 0; i < 10; i++) {
-  setTimeout(num => {
-    console.log(num);
-  }, 0, i);
-}
-console.time('loop time');
-for (let i = 0; i < 10; i++) {}
-console.timeEnd('loop time');
+//4️⃣
+/*
+* The code snippet below is synchronous. It calls callback1 one time and callback2 three times. 
+* You must modify this code to make it asynchronous with the following rules:
+
+1.callback1 must be called only one time, after 2 seconds.
+2.callback2 must be called three times with an interval of 1 second.
+*/
+
+// function job(callback1, callback2) {
+//   let count = 0;
+//   const timer = setInterval(()=>{
+//     callback2();
+//     count++;
+//     if(count === 3){
+//       clearInterval(timer);
+//     }
+//   }, 1000)
+//    setTimeout(()=>{
+//     callback1()
+//   }, 2000)
+// }
+//
+// job(
+//   function () {
+//     console.log('timer1');
+//   },
+//   function () {
+//     console.log('timer2');
+//   }
+// );
+//5️⃣
+// function evenOdd(data) {
+//   return new Promise(function (resolve, reject) {
+//     if (isNaN(data)) reject('error');
+//     if (data % 2 !== 0) {
+//       setTimeout(() => {
+//         resolve('odd');
+//       }, 1000);
+//     }
+//     setTimeout(() => {
+//       reject('even');
+//     }, 2000);
+//   });
+// }
+//
+// const test = function (promise) {
+//   return promise.then(res => console.log(res)).catch(err => console.log(err));
+// };
+// test(evenOdd(5));
+// test(evenOdd(8));
+// test(evenOdd('ko'));
