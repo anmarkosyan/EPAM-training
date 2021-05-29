688d12aa1dda17e18b66f0c63abfb83392a2f80a
anmarkosyan <anush.markos@gmail.com>
2021-05-29 12:34:24 +0400

    new task

--- 18-Promises/script.js
+++ 18-Promises/script.js
@@ -179,29 +179,36 @@ btn.addEventListener('click', function () {
 //   });
 
 //7️⃣
-// const computeSqrtAsync = function (number) {
-//   if (number < 0) {
-//     return Promise.reject('no negative number, please!.');
-//   }
-//
-//   if (number === 0) {
-//     return Promise.resolve(0);
-//   }
-//
-//   return new Promise(function (resolve, reject) {
-//     setTimeout(() => resolve(Math.sqrt(number)), 1000);
-//   });
-// };
-//
-// //START:STATE
-// const forNegative1 = computeSqrtAsync(-1);
-// const forZero = computeSqrtAsync(0);
-// const forSixteen = computeSqrtAsync(16);
-//
-// const testing = function (promise) {
-//   promise.then(data => console.log(data)).catch(err => console.log(err));
-// };
-//
-// testing(forNegative1);
-// testing(forZero);
-// testing(forSixteen);
+const computeSqrtAsync = function (number) {
+  if (number < 0) {
+    return Promise.reject('no negative number, please!.');
+  }
+
+  if (number === 0) {
+    return Promise.resolve(0);
+  }
+
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(Math.sqrt(number)), 1000);
+  });
+};
+
+//START:STATE
+const forNegative1 = computeSqrtAsync(-1);
+const forZero = computeSqrtAsync(0);
+const forSixteen = computeSqrtAsync(16);
+
+const testing = function (promise) {
+  promise.then(
+    function (data) {
+      console.log(data);
+    },
+    function (err) {
+      console.log(err);
+    }
+  );
+};
+
+testing(forNegative1);
+testing(forZero);
+testing(forSixteen);
