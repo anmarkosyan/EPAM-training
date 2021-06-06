a8bb580f99782d555f8bc2bb60ae106f39972543
anmarkosyan <anush.markos@gmail.com>
2021-06-06 22:48:40 +0400

    code signal-matrixElementsSu

--- testPreparation/script.js
+++ testPreparation/script.js
@@ -497,13 +497,67 @@ console.log(parenthesesChecker('{[(])}')); //
  */
 
 //5️⃣Destructuring
-function pointValues(point) {
-  //const { name, age } = { ...point };
-  const { name, age } = point || {};
-  console.log(name);
-  console.log(age);
-}
-let point = { name: 'jerry', age: 2 };
-pointValues(point);
-point = undefined;
-pointValues(point);
+// function pointValues(point) {
+//   //const { name, age } = { ...point };
+//   const { name, age } = point || {};
+//   console.log(name);
+//   console.log(age);
+// }
+// let point = { name: 'jerry', age: 2 };
+// pointValues(point);
+// point = undefined;
+// pointValues(point);
+
+//6️⃣ ========== code signal => matrixElementsSum =========
+/*
+*After becoming famous, the CodeBots decided to move into a new building together.
+* Each of the rooms has a different cost, and some of them are free,
+* but there's a rumour that all the free rooms are haunted!
+* Since the CodeBots are quite superstitious, they refuse to stay in any of the free rooms,
+* or any of the rooms below any of the free rooms.
+
+Given matrix, a rectangular matrix of integers, where each value represents the cost of the room,
+*  your task is to return the total sum of all rooms that are suitable for the CodeBots
+* (ie: add up all the values that don't appear below a 0).
+
+ */
+
+const matrixElementsSum = function (matrix) {
+  // return matrix.map((line, j) => line.map((el, i) => (matrix.slice(0, j).
+  // every(l => l[i] !== 0) ? el : 0))).reduce((a, b) => a + b.reduce((c, d) => c + d), 0);
+  const indexOfGhostHouse = [];
+  let sum = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        indexOfGhostHouse.push(j);
+      } else if (indexOfGhostHouse.indexOf(j) === -1) {
+        sum += matrix[i][j];
+      }
+    }
+  }
+  return sum;
+};
+
+console.log(
+  matrixElementsSum([
+    [0, 1, 1, 2],
+    [0, 5, 0, 0],
+    [2, 0, 3, 3],
+  ])
+); //9 -> 1 + 1 + 2 + 5 = 9
+console.log(
+  matrixElementsSum([
+    [1, 1, 1, 0],
+    [0, 5, 0, 1],
+    [2, 1, 3, 10],
+  ])
+);
+console.log(
+  matrixElementsSum([
+    [1, 1, 1],
+    [2, 2, 2],
+    [3, 3, 3],
+  ])
+); //15
