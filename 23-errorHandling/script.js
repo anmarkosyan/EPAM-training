05551cc5e6c58410ff3fc5e115a18e441717a059
anmarkosyan <anush.markos@gmail.com>
2021-06-12 16:09:04 +0400

    task about errors

--- 23-errorHandling/script.js
+++ 23-errorHandling/script.js
@@ -30,12 +30,15 @@ try {
 //===============  🔴 coding challenges ==================
 
 function reverseString(s) {
-  try {
-    s = s.split('').reverse().join('');
-  } catch {
-    console.log('s.split is not a function');
-  }
+  // try {
+  //   s = s.split('').reverse().join('');
+  // } catch {
+  //   console.log('s.split is not a function');
+  // }
+  //
+  // console.log(s);
 
+   typeof s === 'string' ? s = s.split('').reverse().join('') : console.log('s.split is not a function');
   console.log(s);
 }
 
