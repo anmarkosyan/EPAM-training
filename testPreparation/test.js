b4d875cf2de3a96b381e4ff1515eeae7ede49936
Anush Markosyan <Anush_Markosyan@epam.com>
2022-02-28 16:16:59 +0400

    added some tasks

---
+++ testPreparation/test.js
@@ -0,0 +1,13 @@
+// const arr = [0, 1, 0, 1, 2, 3];
+// const removeDup = new Set(arr);
+// console.log(removeDup);
+//
+// const getName = function(){};
+//
+// const getAge = () => {};
+//
+// function getCoordinates(){}
+
+
+
+
